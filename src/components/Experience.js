8db82a95f218d89f6bf7b57bfbf1e4e9d20aa89c
juhwan1014 <jmoon63@my.bcit.ca>
2021-10-11 16:02:06 -0700

    project page styling

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import pixels from '../images/pixels.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMobileAlt } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 class Experience extends Component {
     state = {  }
@@ -17,16 +19,19 @@ class Experience extends Component {
                          <div className="leftContainer">
                           <div className="imgContainer">
                        <FontAwesomeIcon icon={faMobileAlt} />
-                          {/* <img src={pixels} alt={"pixels"}></img> */}
                         </div>
                         <h2>Title</h2>
-                        <p>asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</p>
+                        <p>asdasdasdasda</p>
                         <span>Projects</span>
                         </div>
                      </div>
-                     <div className="right"></div>
+                     <div className="right">
+                     <img src={pixels} alt={"pixels"}></img>
+                     </div>
                  </div>
              </div>
+             <FontAwesomeIcon icon={faChevronLeft} className="arrow_left"/>
+         <FontAwesomeIcon icon={faChevronRight} className="arrow_right" />
          </div>
          </div>
         </div>  );
