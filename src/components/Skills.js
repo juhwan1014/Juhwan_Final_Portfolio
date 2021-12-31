a36257ee97a5ac6042e186f1dbc24da38f57294f
juhwan1014 <jmoon63@my.bcit.ca>
2021-12-30 17:37:39 -0800

    margin top of skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,82 +7,83 @@ import Fade from 'react-reveal/Fade';
 class Skills extends Component {
     state = {}
     render() {
-        return (<div class="skills" id="skills">
-            {/* <div class="max-width"> */}
-            <h2 class="title">
-            <Fade bottom cascade>My skills</Fade></h2>
+        return (<div className="skills" >
+            <div className="max-width">
+            <h2 className="title">
+            {/* <Fade bottom cascade>My skills</Fade> */}My Skills
+            </h2>
            
-            <div class="skills-content">
+            <div className="skills-content">
          
-                <div class="column left">
-                    <div class="text">
+                <div className="column left">
+                    <div className="text">
                         My creative skills and experiences.</div>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, ratione error est recusandae consequatur, iusto illum deleniti quidem impedit, quos quaerat quis minima sequi. Cupiditate recusandae laudantium esse, harum animi aspernatur quisquam et delectus ipsum quam alias quaerat? Quasi hic quidem illum. Ad delectus natus aut hic explicabo minus quod.</p>
                     <a href="#">Read more</a>
                 </div>
-                <div class="column right">
-                    <div class="bars">
-                        <div class="info">
+                <div className="column right">
+                    <div className="bars">
+                        <div className="info">
                             <span>HTML</span>
                             <span>90%</span>
                         </div>
-                        <div class="line html">
+                        <div className="line html">
                         </div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>CSS</span>
                             <span>70%</span>
                         </div>
-                        <div class="line css">
+                        <div className="line css">
                         </div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>JavaScript</span>
                             <span>50%</span>
                         </div> 
-                        <div class="line js">
+                        <div className="line js">
                         </div>
                     </div>       
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>Java</span>
                             <span>70%</span>
                         </div>
-                        <div class="line java">
+                        <div className="line java">
                         </div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>SQL</span>
                             <span>40%</span>
                         </div>     
-                        <div class="line sql">
+                        <div className="line sql">
                         </div>  
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>React</span>
                             <span>60%</span>
                         </div>
-                        <div class="line react">
+                        <div className="line react">
                         </div>
                     </div>
-                    <div class="bars">
-                        <div class="info">
+                    <div className="bars">
+                        <div className="info">
                             <span>Python/C#/.net</span>
                             <span>30%</span>
                         </div>
-                        <div class="line etc">
+                        <div className="line etc">
                         </div>
                     </div>
                     </div>
                     
                 </div>
           
-            {/* </div> */}
+            </div>
         </div>
         );
     }
