c9f64b06357c36e7aeea4f4e02671a9908d21258
juhwan1014 <jmoon63@my.bcit.ca>
2022-01-03 18:44:58 -0800

    add one more page

---
+++ src/components/Teams.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import me from '../images/me.jpeg'
+
+class Teams extends Component {
+    state = {}
+
+    componentDidMount() {
+        
+        $('carousel').owlCarousel({
+
+        });
+    }
+
+
+    render() {
+        return (<div className='teams'>
+
+            <div className='max-width'>
+                <h2 className='title'>My Teams</h2>
+                <div className='carousel owl-carousel'>
+                    <div className='card'>
+                        <div className='box'>
+                            <img src={me} alt={"me"}></img>
+                            <div className='text'>내가 바로 문주환</div>
+                            <p>asd dlasd sladasldkasldk sadjaksd osdjaksdksiqd sdaokd oqdqosdkqmdksqmd sqodj</p>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='box'>
+                        <img src={me} alt={"me"}></img>
+                        <div className='text'>내가 바로 문주환</div>
+                        <p>asd dlasd sladasldkasldk sadjaksd osdjaksdksiqd sdaokd oqdqosdkqmdksqmd sqodj</p>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='box'>
+                        <img src={me} alt={"me"}></img>
+                        <div className='text'>내가 바로 문주환</div>
+                        <p>asd dlasd sladasldkasldk sadjaksd osdjaksdksiqd sdaokd oqdqosdkqmdksqmd sqodj</p>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='box'>
+                        <img src={me} alt={"me"}></img>
+                        <div className='text'>내가 바로 문주환</div>
+                        <p>asd dlasd sladasldkasldk sadjaksd osdjaksdksiqd sdaokd oqdqosdkqmdksqmd sqodj</p>
+                        </div>
+                    </div>
+                    <div className='card'>
+                        <div className='box'>
+                        <img src={me} alt={"me"}></img>
+                        <div className='text'>내가 바로 문주환</div>
+                        <p>asd dlasd sladasldkasldk sadjaksd osdjaksdksiqd sdaokd oqdqosdkqmdksqmd sqodj</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>);
+    }
+}
+
+export default Teams;
+
+
