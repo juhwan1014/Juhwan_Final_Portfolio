c2639f1e67f7b81cbb8f144655b18ea722185963
juhwan1014 <jmoon63@my.bcit.ca>
2022-11-25 17:27:40 -0800

    add animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { Component, useEffect, useRef } from 'react';
 import { init } from 'ityped'
+import Lottie from "lottie-react";
+import steam from "./steam.json";
 
 // import {  } from "react";
 
@@ -19,11 +21,18 @@ class Header extends Component {
         return (
         
         <div className='header'>
-
+                            <div className="steam">
+                                <Lottie animationData={steam} loop={true} />
+                            </div>
             <div className="max-width">
             {/* <h1 className='heading-background'>POSSIBLE</h1> */}
+           
                 <div className="header-content">
-                    <div className="text_1">Hello, my name is</div>
+                    
+                           
+
+                            <div className="text_1">Hello, my name is</div>
+                   
                     <div className="text_2">Juhwan Moon</div>
                     <div className="text_3">I'm a <span id="myElement"></span></div>
                     <a href={`mailto:$juhwan0718@gmail.com`} rel="noopenner norefferer">Contact</a>
